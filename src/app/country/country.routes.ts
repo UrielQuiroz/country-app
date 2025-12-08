c63fd0df09ce533f5043243e205293c73e55ecc2
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-08 17:00:29 -0600

    Rutas y estructura de carpetas

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page';
+
+export const countryroutes: Routes = [
+  {
+    path: '',
+    component: ByCapitalPageComponent
+  },
+];
+
+export default countryroutes;
