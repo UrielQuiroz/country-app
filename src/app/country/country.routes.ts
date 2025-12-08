77547e07c8ff7d833ce0beac8dff212f655a4c8f
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-08 17:24:39 -0600

    Layout Components

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
+import { CountryLayoutComponent } from './layouts/CountryLayout/CountryLayout';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page';
 
 export const countryroutes: Routes = [
   {
     path: '',
-    component: ByCapitalPageComponent
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      }
+    ]
   },
 ];
 
