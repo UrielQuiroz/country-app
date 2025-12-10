b60de742e8d04849d580c878e476b7211fd4cdb8
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-09 18:18:50 -0600

    country.service para consumir api de REST Countries y creando interfaz de la respuesta

--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CountryListComponent } from "../../components/country-list/country-list";
 import { SearchInputComponent } from "../../components/search-input/search-input";
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,7 +9,11 @@ import { SearchInputComponent } from "../../components/search-input/search-input
   templateUrl: './by-capital-page.html',
 })
 export class ByCapitalPageComponent {
-  onSearch(value: string) {
-    console.log({ value })
+  countryService = inject(CountryService);
+  onSearch(query: string) {
+    console.log({ query })
+
+    this.countryService.searchByCapital(query).
+      subscribe(res => console.log(res))
   }
 }
