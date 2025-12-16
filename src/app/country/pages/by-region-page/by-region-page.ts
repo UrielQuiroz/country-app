63e47a83e126acb8404ccd751eac73f94e4c63ad
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-15 19:47:05 -0600

    Se implemento búsqueda de paises por región con consumo de API, caché y visualización

--- src/app/country/pages/by-region-page/by-region-page.ts
+++ src/app/country/pages/by-region-page/by-region-page.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CountryListComponent } from "../../components/country-list/country-list";
+import { Region } from '../../interfaces/region.type';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-region-page',
   imports: [CountryListComponent],
   templateUrl: './by-region-page.html',
 })
-export class ByRegionPageComponent { }
+export class ByRegionPageComponent {
+
+  countryService = inject(CountryService);
+
+    public regions: Region[] = [
+      'Africa',
+      'Americas',
+      'Asia',
+      'Europe',
+      'Oceania',
+      'Antarctic',
+    ];
+
+    selectedRegion = signal<Region | null>(null);
+
+    countryResource = rxResource({
+      params: () => ({ region: this.selectedRegion() }),
+
+      stream: ({ params  }) => {
+        if(!params.region) return of([]);
+
+        return this.countryService.searchByRegion(params.region)
+      },
+    })
+}
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -5,6 +5,7 @@ import { RESTCountry } from '../interfaces/rest-countries.interface';
 import { catchError, delay, map, Observable, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../mapper/country.mapper';
 import type { Country } from '../interfaces/country.interface';
+import { Region } from '../interfaces/region.type';
 
 const API_URL = 'https://restcountries.com/v3.1'
 
@@ -15,6 +16,7 @@ export class CountryService {
   private http = inject(HttpClient);
   private queryCacheCapital = new Map<string, Country[]>();
   private queryCacheCountry = new Map<string, Country[]>();
+  private queryCacheRegion = new Map<Region, Country[]>();
 
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
@@ -65,6 +67,25 @@ export class CountryService {
         catchError((error) => {
           return throwError(
             () => new Error(`No se pudo obtener paises con ese código ${ code }`)
+          )
+        })
+      )
+  }
+
+
+    searchByRegion(region: Region) {
+
+    if(this.queryCacheRegion.has(region)) {
+      return of(this.queryCacheRegion.get(region) ?? []);
+    }
+
+    return this.http.get<RESTCountry[]>(`${ API_URL }/region/${ region }`)
+      .pipe(
+        map(resp => CountryMapper.mapRestCountryArrayToCountryArray(resp) ),
+        tap((countries) => this.queryCacheRegion.set(region, countries)),
+        catchError((error) => {
+          return throwError(
+            () => new Error(`No se pudo obtener paises con esta búsqueda ${ region }`)
           )
         })
       )
