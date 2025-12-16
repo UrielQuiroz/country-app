deb4970297cc8cbafdc2eb7b2e02aa8a187a2b58
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-16 12:13:00 -0600

    Cambiar queryParams vía código para busquedas por pais

--- src/app/country/pages/by-country-page/by-country-page.ts
+++ src/app/country/pages/by-country-page/by-country-page.ts
@@ -1,9 +1,10 @@
-import { Component, inject, resource, signal } from '@angular/core';
+import { Component, inject, linkedSignal, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input";
 import { CountryListComponent } from "../../components/country-list/country-list";
 import { CountryService } from '../../services/country.service';
 import { firstValueFrom, of } from 'rxjs';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-by-country-page',
@@ -14,13 +15,24 @@ export class ByCountryPageComponent {
 
     countryService = inject(CountryService);
 
-    query = signal('');
+    activatedRoute = inject(ActivatedRoute);
+    route = inject(Router);
+
+    queryParams = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
+
+    query = linkedSignal(() => this.queryParams);
 
     countryResource = rxResource({
       params: () => ({ query: this.query() }),
 
       stream: ({ params  }) => {
         if(!params.query) return of([]);
+
+      this.route.navigate(['/country/by-country/'], {
+        queryParams: {
+          query: params.query
+        }
+      })
 
         return this.countryService.searchByCountry(params.query)
       },
